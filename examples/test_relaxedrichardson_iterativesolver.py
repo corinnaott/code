60f0120651a7f802019e8c4d45d06a390c49bee9
corinnaott <s4coottt@uni-trier.de>
2022-03-30 00:27:20 +0200

    ready to go

--- examples/test_relaxedrichardson_iterativesolver.py
+++ examples/test_relaxedrichardson_iterativesolver.py
@@ -2,28 +2,33 @@ from src import relaxedrichardson_iterativesolver as rr
 print("relaxedrichardson_iterativesolver:")
 
 n = 5
+i=0
 
-
-A1 = rr.tri.tridiagtoep(n,[2* n**2, -1* n**2, -1* n**2] )
+A1 = rr.tri.tridiagtoep(n,[2+i* n**2, -1* n**2, -1* n**2])
 b = [1]*n
 x0 = [0]*n
 print("matrix A1 =",rr.csr.csr_matrix.toarray(A1))
 #print(A1.data, A1.indices, A1.indptr)
 
-#print(rr.richardson(A1, b, x0, theta=.1, maxiter=500, tol=1e-08))  #overflow error: result too large => too many loops?
-print("for matrix A1, dimension n=5, vector b=[1]*n, start vector x0=[0]*n, relaxation theta=0.001, error tolerance tol=tol=1e-08 (default): ")
+#❗️#print(rr.richardson(A1, b, x0, theta=.1, maxiter=500, tol=1e-08))  #overflow error: result too large => too many loops?
+print("for matrix A1, dimension n=5, vector b=[1]*n, start vector x0=[0]*n, relaxation theta=0.001, error tolerance tol=1e-08 (default): ")
 print(rr.richardson(A1, b, x0, theta=.001, maxiter=500, tol=1e-08))    #changed relaxation #converges for n<28
 
-print("-----")
-help(rr.richardson)
+print("----------")
 #unmatching dimensions
+print("for matrix A1 with dimension n=5 and vector c=[1]*(n-1)")
 c= [1]*(n-1)
-#print(rr.richardson(A1, c, x0, theta=.001, maxiter=500, tol=1e-08))
+print(rr.richardson(A1, c, x0, theta=.001, maxiter=500, tol=1e-08))
 
+print("---------")
 #A2
-n = 5
-delta = 1
-A2 = A1 + rr.uni.I(n, delta)
-#print(rr.richardson(A2, b, x0, theta=.001, maxiter=500, tol=1e-08))
+n = 4
+x0 = [0]*n
+b = [1]*n
+#delta = 10
+i= 63
+#A2 = A1 + rr.uni.I(n, delta)
+A2 =  rr.tri.tridiagtoep(n,[2+i* n**2, -1* n**2, -1* n**2])
+print("A2= A1 + delta * I_n. for n=4 and delta=63")
+print(rr.richardson(A2, b, x0, theta=.001, maxiter=500, tol=1e-08))
 
-#help(rr.richardson)
--- main.py
+++ main.py
@@ -5,28 +5,3 @@ from examples import test_linalg
 
 from examples import test_relaxedrichardson_iterativesolver
     #, test_relaxedrichardson_iterativesolver, test_tridiagtoep, test_unitymatrix
-
-#--------------------------------------
-
-#test class csr_matrix
-#print("test csr_matrix:")
-#print("__init__:","data list:", A.data, ", indices list:", A.indices,", indptr list", A.indptr,", shape tuple",  A.shape)
-#print("toarray():","full matrix A:", csr.csr_matrix.toarray(A), ",full matrix B:", csr.csr_matrix.toarray(B))
-#print("__matmul__ / @ operator:", csr.csr_matrix.__matmul__(A,b), "equals" , A@b)
-#print("__add__ / + operator:" , csr.csr_matrix.toarray(csr.csr_matrix.__add__(A, B)), "equals", csr.csr_matrix.toarray(A+B))
-
-#test linalg
-##vector
-#print("vector(): v=" ,v )
-#print("vecdim(): vecdim(v)=" , la.vecdim(v))
-##vector dimension #alpha=2.5
-#print("scal(): scal(alpha, v)=", la.scal(alpha, v))
-##vector addition
-#print("axpy(): only vectors: axpy(v, w [,alpha=1.0])=",la.axpy(v,w),"/ with scalar alpha: axpy(v, w, alpha)=", la.axpy(v, w, alpha))
-##norm #x = la.vector(5 ,3, 2**0.5)
-#print("nrm(): nrm(x)=", la.nrm(x) , "  ( = (5**2 + 3**2 + (2**0.5)**2)**0.5 = (25+9+2)**0.5 = 36**0.5  )" )
-##matrix vector multiplication
-#print("csrvm(): csrvm(A2, x2)=", la.csrmv(A2, x2))
-
-
-
